2dfca85889203339e71f1f0221723cce7cea9b47
Gumann <henninggu3003@gmail.com>
2017-05-12 15:52:30 +0200

    Added comments.

--- src/de/gruppe3/projekt2/Validator.java
+++ src/de/gruppe3/projekt2/Validator.java
@@ -6,8 +6,9 @@ import java.time.Instant;
 import java.util.Date;
 
 /**
- * Created by guhennin on 12.05.17.
+ * Created by Henning on 12.05.17.
  */
+
 public class Validator {
     public static InputManager.ValidationMethod valBirthday =
             (object) -> validateBirthday((String) object);
@@ -30,7 +31,7 @@ public class Validator {
         try {
             Date date = format.parse(birthday);
 
-            if (date.after(Date.from(Instant.now())))
+            if (date.after(Date.from(Instant.now())))       //Students typically aren't older than 40-50 and can't be born in the future.
                 return false;
 
             if (date.before(format.parse("01.01.1970")))
@@ -43,6 +44,11 @@ public class Validator {
         return true;
     }
 
+    /**
+     * Checks if the name has at least one uppercase and several lowercase letters and if there is a dash.
+     * @param name uppercase and lowercase letters and dash; A-Z, a-z, '-'
+     * @return true
+     */
     static boolean validateName(String name) {
         if (name.matches("(\\p{Lu}\\p{Ll}+)([\\- ](\\p{Lu}\\p{Ll}+))+"))
             return true;
@@ -50,6 +56,11 @@ public class Validator {
             return false;
     }
 
+    /**
+     * Checks if the subject consists of letters and numbers and returns true.
+     * @param subject uppercase and lowercase letters and numbers from 0-9; A-Z, a-z, 0-9
+     * @return
+     */
     static boolean validateSubject(String subject) {
         if (subject.matches("(\\p{Lu}\\p{Ll}+)( ([\\p{L}0-9]+))*")) {
             return true;
@@ -57,6 +68,11 @@ public class Validator {
             return false;
     }
 
+    /**
+     * Checks if the grades of a student are below 5.0 and above 1.0. If both statemnts are true, then it returns true.
+     * @param grades values 1.0-5.0
+     * @return true
+     */
     static boolean validateGrade(double grades) {
         if (grades <= 5.0 && grades >= 1.0)
             return true;
